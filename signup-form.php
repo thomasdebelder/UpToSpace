ed594d45c483db1216bc459d6d18852c01f8b092
thomasdebelder <thomasdebelder95@gmail.com>
2018-05-14 14:41:50 +0200

    Feature 1: Register
    HTML

---
+++ signup-form.php
@@ -0,0 +1,25 @@
+
+<form method="post">
+
+	<h3>Sign up </h3>
+	<ul>
+		<li>
+		    <input type="text" name="screenName" class="form-control" placeholder="Full Name"/>
+		</li>
+		<li>
+		    <input type="email" name="email" class="form-control" placeholder="Email"/>
+		</li>
+		<li>
+			<input type="password" name="password" class="form-control" placeholder="Password"/>
+		</li>
+
+        <li>
+            <input type="password" name="passwordConfirm" class="form-control" placeholder="Confirm Password"/>
+        </li>
+		<li>
+			<input type="submit" name="signup" Value="Register">
+		</li>
+
+	</ul>
+
+</form>
