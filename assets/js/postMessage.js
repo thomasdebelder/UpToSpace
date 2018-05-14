cf08495ac2636e55c7d549b42ab47d6361cb3be9
Sarah Van Oers <vanoerssarah@gmail.com>
2018-05-14 22:56:59 +0200

    popup

--- assets/js/postMessage.js
+++ assets/js/postMessage.js
@@ -2,7 +2,7 @@ $(function(){
 	$(document).on('click', '#send', function(){
 		var message = $('#msg').val();
 		var get_id   = $(this).data('user');
-		$.post('http://localhost:8888/upToSpace/core/ajax/messages.php', {sendMessage:message,get_id:get_id}, function(data){
+		$.post('http://localhost:8888/UpToSpace/core/ajax/messages.php', {sendMessage:message,get_id:get_id}, function(data){
 			getMessages();
 			$('#msg').val('');
 		});
