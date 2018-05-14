e417de6c620e3528b84848d28309a4d19ab27f3e
Sarah Van Oers <vanoerssarah@gmail.com>
2018-05-14 15:03:07 +0200

    databank connectie
    -databank naam = space
    -vergeet niet bij windows password weg te halen.

---
+++ databanken/connection.php
@@ -0,0 +1,10 @@
+<?php
+    $db = 'mysql:host=localhost; dbname=space';
+	$user = 'root';
+    $password = 'root';
+    
+    try{
+		$pdo = new PDO($db, $user, $password);
+	}catch(PDOException $e){
+		echo 'connection error! ' . $e;
+    }
