a094ea2fce03ccbe763cad9f1e95a00198bc4685
Sarah Van Oers <vanoerssarah@gmail.com>
2018-05-14 15:20:28 +0200

    global pdo

--- core/init.php
+++ core/init.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
-	include 'database/connection.php';
+    include_once('database/connection.php');
+    //je kan globale variabelen vanaf elke plek oproepen (ook vanuit functies of methodes)
+    global $pdo;
 
 
 
     // gevonden in een tutarial zo moet je niet over de poort veranderen 
     // BASE_URL overal zetten en het werkt
     define('BASE_URL', 'http://localhost:8888/twitter/');
-?>
