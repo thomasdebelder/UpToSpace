9f06a40db6222fd37f0a0352f782ae237713fa8f
Sarah Van Oers <vanoerssarah@gmail.com>
2018-05-14 17:41:15 +0200

    nav home

--- home.php
+++ home.php
@@ -1,1 +1,48 @@
-<h1>Hello</h1>
+<!DOCTYPE HTML>
+ <html>
+	<head>
+		<title>Homepage</title>
+		  <meta charset="UTF-8" />
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
+ 	  	  <link rel="stylesheet" href="assets/css/style-complete.css"/>
+   		  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	</head>
+	<!--Helvetica Neue-->
+<body>
+<div class="wrapper">
+<!-- header wrapper -->
+<div class="header-wrapper">
+
+<div class="nav-container">
+	<!-- Nav -->
+	<div class="nav">
+      <div class="nav-right">
+        <ul>
+		  <li class="hover"><label class="drop-label" for="drop-wrap1"><img src="<?php echo $user->profileImage; ?>"/></label>
+		  <input type="checkbox" id="drop-wrap1">
+		    <div class="drop-wrap">
+		      <div class="drop-inner">
+		        <ul>
+		          <li><a href="home.php">Home</a></li>
+	              <li><a href="profileEdit.php">Profile</a></li>
+                  <li id="messagePopup"><a>Messages<span id="messages"><?php if($notify->totalM > 0){echo '<span class="span-i">'.$notify->totalM.'</span>';}?></span></a></li>
+                  <li><a href="account.php">Settings</a></li>
+				  <li><a href="logout.php">Log out</a></li>
+				</ul>
+		      </div>
+		    </div>
+		  </li>
+		  <li>
+            <input type="text" placeholder="Search" class= "search"/>
+		    <div class="search-result"></div>
+		  </li>
+		  <li>
+		    <label class="addTweetBtn">New Post</label></li>
+		</ul>
+		</div><!-- nav right ends-->
+
+	</div><!-- nav ends -->
+
+</div><!-- nav container ends -->
+
+</div><!-- header wrapper end -->
