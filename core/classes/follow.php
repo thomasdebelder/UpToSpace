efd171fc56fd27e6a86f7326435357beffdcb4f5
thomasdebelder <thomasdebelder95@gmail.com>
2018-05-14 19:51:25 +0200

    Feature 11 + extra feature
    gebruikers followen + extra unfollowen ook mogelijk

--- core/classes/follow.php
+++ core/classes/follow.php
@@ -1,171 +1,171 @@
-<?php
-class Follow extends User {
-  protected $message;
-  
-  public function __construct($pdo) {
-    $this->pdo = $pdo;
-    $this->message = new Message($this->pdo);
-  }
-
-  public function checkFollow($followerID, $user_id) {
-    $stmt = $this->pdo->prepare("SELECT * FROM `follow` WHERE `sender` = :user_id  AND `receiver` = :followerID");
-    $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
-    $stmt->bindParam(":followerID", $followerID, PDO::PARAM_INT);
-    $stmt->execute();
+	<?php
+	class Follow extends User {
+		protected $message;
 		
-    return $stmt->fetch(PDO::FETCH_ASSOC);
-  }
-  
-  public function followBtn($profileID, $user_id, $followID) {
-    $data = $this->checkFollow($profileID, $user_id);
+		public function __construct($pdo) {
+			$this->pdo = $pdo;
+			$this->message = new Message($this->pdo);
+		}
+
+		public function checkFollow($followerID, $user_id) {
+			$stmt = $this->pdo->prepare("SELECT * FROM `follow` WHERE `sender` = :user_id  AND `receiver` = :followerID");
+			$stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+			$stmt->bindParam(":followerID", $followerID, PDO::PARAM_INT);
+			$stmt->execute();
+			
+			return $stmt->fetch(PDO::FETCH_ASSOC);
+		}
 		
-    if($this->loggedIn()===true) {
+		public function followBtn($profileID, $user_id, $followID) {
+			$data = $this->checkFollow($profileID, $user_id);
+			
+			if($this->loggedIn()===true) {
 
-      if($profileID != $user_id) {
-        if($data['receiver'] == $profileID) {
-		  //Following btn
-		  
-          return "<button class='f-btn following-btn follow-btn' data-follow='".$profileID."' data-profile='".$followID."'>Following</button>";
+				if($profileID != $user_id) {
+					if($data['receiver'] == $profileID) {
+				//Following btn
+				
+						return "<button class='f-btn following-btn follow-btn' data-follow='".$profileID."' data-profile='".$followID."'>Following</button>";
+			}
+					else {
+						//Follow button
+						return "<button class='f-btn follow-btn' data-follow='".$profileID."' data-profile='".$followID."'><i class='fa fa-user-plus'></i>Follow</button>";
+					}
+				}
+			else {
+				//edit button
+				return "<button class='f-btn' onclick=location.href='profileEdit.php'>Edit Profile</button>";
+			}
+			}
+			else {
+				return "<button class='f-btn' onclick=location.href='index.php'><i class='fa fa-user-plus'></i>Follow</button>";
+			}
 		}
-        else {
-          //Follow button
-          return "<button class='f-btn follow-btn' data-follow='".$profileID."' data-profile='".$followID."'><i class='fa fa-user-plus'></i>Follow</button>";
-        }
-      }
-    else {
-      //edit button
-      return "<button class='f-btn' onclick=location.href='profileEdit.php'>Edit Profile</button>";
-    }
-    }
-    else {
-      return "<button class='f-btn' onclick=location.href='index.php'><i class='fa fa-user-plus'></i>Follow</button>";
-    }
-  }
-  public function follow($followID, $user_id, $profileID) {
-    $this->create('follow', array('sender' => $user_id, 'receiver' => $followID, 'followOn' => date("Y-M-D H:i:s")));
-    $this->addFollowCount($followID, $user_id);
-    $stmt = $this->pdo->prepare('SELECT `user_id`, `following`, `followers` FROM `users` LEFT JOIN `follow` ON `sender` = :user_id AND CASE WHEN `receiver` = :user_id THEN `sender` = `user_id` END WHERE `user_id` = :profileID');
-    $stmt->execute(array("user_id" => $user_id,"profileID" => $profileID));
-    $data = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    echo json_encode($data);
-    //This fixed php 7 error
-    $this->message->sendNotification($followID, $user_id, $user_id, 'follow');
- 
-  }
-
-  public function unfollow($followID, $user_id, $profileID) {
-    $this->delete('follow', array('sender' => $user_id, 'receiver' => $followID));
-    $this->removeFollowCount($followID, $user_id);
-    $stmt = $this->pdo->prepare('SELECT `user_id`, `following`, `followers` FROM `users` LEFT JOIN `follow` ON `sender` = :user_id AND CASE WHEN `receiver` = :user_id THEN `sender` = `user_id` END WHERE `user_id` = :profileID');
-    $stmt->execute(array("user_id" => $user_id,"profileID" => $profileID));
-    $data = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    echo json_encode($data);
-  }
-  
-  public function addFollowCount( $followID, $user_id) {
-    $stmt = $this->pdo->prepare("UPDATE `users` SET `following` = `following` + 1 WHERE `user_id` = :user_id; UPDATE `users` SET `followers` = `followers` + 1 WHERE `user_id` = :followID");
-    $stmt->execute(array("user_id" => $user_id, "followID" => $followID));
-  }
-
-  public function removeFollowCount($followID, $user_id) {
-    $stmt = $this->pdo->prepare("UPDATE `users` SET `following` = `following` - 1 WHERE `user_id` = :user_id; UPDATE `users` SET `followers` = `followers` - 1 WHERE `user_id` = :followID");
-    $stmt->execute(array("user_id" => $user_id, "followID" => $followID));
-  }
-  
-  public function followingList($profileID, $user_id, $followID) {
-    $stmt = $this->pdo->prepare("SELECT * FROM `users` LEFT JOIN `follow` ON `receiver` = `user_id` AND CASE WHEN `sender` = :profileID THEN `receiver` = `user_id` END WHERE `sender` IS NOT NULL ");
-    $stmt->bindParam(":profileID", $profileID, PDO::PARAM_INT);
-    $stmt->execute();
-    $followings = $stmt->fetchAll(PDO::FETCH_OBJ);
-    
-    foreach ($followings as $following) {
-      echo '<div class="follow-unfollow-box">
-		    <div class="follow-unfollow-inner">
-		    <div class="follow-background">
-		    <img src="'.$following->profileCover.'"/>
-		    </div>
-		    <div class="follow-person-button-img">
-							<div class="follow-person-img"> 
-							 	<img src="'.$following->profileImage.'"/>
-							</div>
-							<div class="follow-person-button">
-								 '.$this->followBtn($following->user_id, $user_id, $followID).'
-						    </div>
-						</div>
-						<div class="follow-person-bio">
-							<div class="follow-person-name">
-								<a href="'.$following->username.'">'.$following->screenName.'</a>
-							</div>
-							<div class="follow-person-tname">
-								<a href="'.$following->username.'">@'.$following->username.'</a>
-							</div>
-							<div class="follow-person-dis">
-								'.Tweet::getTweetLinks($following->bio).'
-							</div>
-						</div>
-					</div>
-				</div>';
+		public function follow($followID, $user_id, $profileID) {
+			$this->create('follow', array('sender' => $user_id, 'receiver' => $followID, 'followOn' => date("Y-M-D H:i:s")));
+			$this->addFollowCount($followID, $user_id);
+			$stmt = $this->pdo->prepare('SELECT `user_id`, `following`, `followers` FROM `users` LEFT JOIN `follow` ON `sender` = :user_id AND CASE WHEN `receiver` = :user_id THEN `sender` = `user_id` END WHERE `user_id` = :profileID');
+			$stmt->execute(array("user_id" => $user_id,"profileID" => $profileID));
+			$data = $stmt->fetch(PDO::FETCH_ASSOC);
+			
+			echo json_encode($data);
+			//This fixed php 7 error
+			$this->message->sendNotification($followID, $user_id, $user_id, 'follow');
+	
 		}
-	}
 
-	public function followersList($profileID, $user_id, $followID) {
-		$stmt = $this->pdo->prepare("SELECT * FROM `users` LEFT JOIN `follow` ON `sender` = `user_id` AND CASE WHEN `receiver` = :profileID THEN `sender` = `user_id` END WHERE `user_id` and `receiver` IS NOT NULL");
-		$stmt->bindParam(":profileID", $profileID, PDO::PARAM_INT);
-		$stmt->execute();
-		$followings = $stmt->fetchAll(PDO::FETCH_OBJ);
-		foreach ($followings as $following) {
-			echo '<div class="follow-unfollow-box">
+		public function unfollow($followID, $user_id, $profileID) {
+			$this->delete('follow', array('sender' => $user_id, 'receiver' => $followID));
+			$this->removeFollowCount($followID, $user_id);
+			$stmt = $this->pdo->prepare('SELECT `user_id`, `following`, `followers` FROM `users` LEFT JOIN `follow` ON `sender` = :user_id AND CASE WHEN `receiver` = :user_id THEN `sender` = `user_id` END WHERE `user_id` = :profileID');
+			$stmt->execute(array("user_id" => $user_id,"profileID" => $profileID));
+			$data = $stmt->fetch(PDO::FETCH_ASSOC);
+			
+			echo json_encode($data);
+		}
+		
+		public function addFollowCount( $followID, $user_id) {
+			$stmt = $this->pdo->prepare("UPDATE `users` SET `following` = `following` + 1 WHERE `user_id` = :user_id; UPDATE `users` SET `followers` = `followers` + 1 WHERE `user_id` = :followID");
+			$stmt->execute(array("user_id" => $user_id, "followID" => $followID));
+		}
+
+		public function removeFollowCount($followID, $user_id) {
+			$stmt = $this->pdo->prepare("UPDATE `users` SET `following` = `following` - 1 WHERE `user_id` = :user_id; UPDATE `users` SET `followers` = `followers` - 1 WHERE `user_id` = :followID");
+			$stmt->execute(array("user_id" => $user_id, "followID" => $followID));
+		}
+		
+		public function followingList($profileID, $user_id, $followID) {
+			$stmt = $this->pdo->prepare("SELECT * FROM `users` LEFT JOIN `follow` ON `receiver` = `user_id` AND CASE WHEN `sender` = :profileID THEN `receiver` = `user_id` END WHERE `sender` IS NOT NULL ");
+			$stmt->bindParam(":profileID", $profileID, PDO::PARAM_INT);
+			$stmt->execute();
+			$followings = $stmt->fetchAll(PDO::FETCH_OBJ);
+			
+			foreach ($followings as $following) {
+				echo '<div class="follow-unfollow-box">
 					<div class="follow-unfollow-inner">
-						<div class="follow-background">
-							<img src="'.$following->profileCover.'"/>
-						</div>
-						<div class="follow-person-button-img">
-							<div class="follow-person-img"> 
-							 	<img src="'.$following->profileImage.'"/>
-							</div>
-							<div class="follow-person-button">
-								 '.$this->followBtn($following->user_id, $user_id, $followID).'
-						    </div>
-						</div>
-						<div class="follow-person-bio">
-							<div class="follow-person-name">
-								<a href="'.$following->username.'">'.$following->screenName.'</a>
-							</div>
-							<div class="follow-person-tname">
-								<a href="'.$following->username.'">@'.$following->username.'</a>
-							</div>
-							<div class="follow-person-dis">
-								'.Tweet::getTweetLinks($following->bio).'
-							</div>
-						</div>
+					<div class="follow-background">
+					<img src="'.$following->profileCover.'"/>
 					</div>
-				</div>';
+					<div class="follow-person-button-img">
+								<div class="follow-person-img"> 
+									<img src="'.$following->profileImage.'"/>
+								</div>
+								<div class="follow-person-button">
+									'.$this->followBtn($following->user_id, $user_id, $followID).'
+									</div>
+							</div>
+							<div class="follow-person-bio">
+								<div class="follow-person-name">
+									<a href="'.$following->username.'">'.$following->screenName.'</a>
+								</div>
+								<div class="follow-person-tname">
+									<a href="'.$following->username.'">@'.$following->username.'</a>
+								</div>
+								<div class="follow-person-dis">
+									'.Tweet::getTweetLinks($following->bio).'
+								</div>
+							</div>
+						</div>
+					</div>';
+			}
 		}
-	}
 
-	public function whoToFollow($user_id, $profileID) {
-      $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` != :user_id AND `user_id` NOT IN (SELECT `receiver` FROM `follow` WHERE `sender` = :user_id) ORDER BY rand() LIMIT 3");
-      $stmt->execute(array("user_id" => $user_id));
-      $users = $stmt->fetchAll(PDO::FETCH_OBJ);
-      echo '<div class="follow-wrap"><div class="follow-inner"><div class="follow-title"><h3>Who to follow</h3></div>';
-      
-      foreach ($users as $user) {
-        echo '<div class="follow-body">
-					<div class="follow-img">
-					  <img src="'.$user->profileImage.'"/>
-				    </div>
-					<div class="follow-content">
-						<div class="fo-co-head">
-							<a href="'.$user->username.'">'.$user->screenName.'</a><span>@'.$user->username.'</span>
+		public function followersList($profileID, $user_id, $followID) {
+			$stmt = $this->pdo->prepare("SELECT * FROM `users` LEFT JOIN `follow` ON `sender` = `user_id` AND CASE WHEN `receiver` = :profileID THEN `sender` = `user_id` END WHERE `user_id` and `receiver` IS NOT NULL");
+			$stmt->bindParam(":profileID", $profileID, PDO::PARAM_INT);
+			$stmt->execute();
+			$followings = $stmt->fetchAll(PDO::FETCH_OBJ);
+			foreach ($followings as $following) {
+				echo '<div class="follow-unfollow-box">
+						<div class="follow-unfollow-inner">
+							<div class="follow-background">
+								<img src="'.$following->profileCover.'"/>
+							</div>
+							<div class="follow-person-button-img">
+								<div class="follow-person-img"> 
+									<img src="'.$following->profileImage.'"/>
+								</div>
+								<div class="follow-person-button">
+									'.$this->followBtn($following->user_id, $user_id, $followID).'
+									</div>
+							</div>
+							<div class="follow-person-bio">
+								<div class="follow-person-name">
+									<a href="'.$following->username.'">'.$following->screenName.'</a>
+								</div>
+								<div class="follow-person-tname">
+									<a href="'.$following->username.'">@'.$following->username.'</a>
+								</div>
+								<div class="follow-person-dis">
+									'.Tweet::getTweetLinks($following->bio).'
+								</div>
+							</div>
 						</div>
-						<!-- FOLLOW BUTTON -->
-						'.$this->followBtn($user->user_id, $user_id, $profileID).'
-					</div>
-				</div>';
-      }
-      echo '</div></div>';
-    }
-}
-?>
+					</div>';
+			}
+		}
+
+		public function whoToFollow($user_id, $profileID) {
+				$stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` != :user_id AND `user_id` NOT IN (SELECT `receiver` FROM `follow` WHERE `sender` = :user_id) ORDER BY rand() LIMIT 3");
+				$stmt->execute(array("user_id" => $user_id));
+				$users = $stmt->fetchAll(PDO::FETCH_OBJ);
+				echo '<div class="follow-wrap"><div class="follow-inner"><div class="follow-title"><h3>Who to follow</h3></div>';
+				
+				foreach ($users as $user) {
+					echo '<div class="follow-body">
+						<div class="follow-img">
+							<img src="'.$user->profileImage.'"/>
+							</div>
+						<div class="follow-content">
+							<div class="fo-co-head">
+								<a href="'.$user->username.'">'.$user->screenName.'</a><span>@'.$user->username.'</span>
+							</div>
+							<!-- FOLLOW BUTTON -->
+							'.$this->followBtn($user->user_id, $user_id, $profileID).'
+						</div>
+					</div>';
+				}
+				echo '</div></div>';
+			}
+	}
+	?>
---
+++ following.php
@@ -0,0 +1,243 @@
+<?php 
+ 	if(isset($_GET['username']) === true && empty($_GET['username']) === false){
+		include 'core/init.php';
+		$username    = $getFromU->checkInput($_GET['username']);
+		$profileId   = $getFromU->userIdByUsername($username);
+		$profileData = $getFromU->userData($profileId);
+		$user_id 	 = $_SESSION['user_id'];
+		$user 		 = $getFromU->userData($user_id);
+
+ 		if($getFromU->loggedIn() === false){
+			header('Location: index.php');
+		}	
+		if(!$profileData){
+			header('Location: index.php');
+		}
+
+	}else{
+		header('Location: index.php');
+	}
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>People followed by <?php echo $profileData->screenName. ' (@'.$profileData->username.')';?></title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
+		<link rel="stylesheet" href="assets/css/style-complete.css"/>
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js"  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+   </head>
+<!--Helvetica Neue-->
+<body>
+<div class="wrapper">
+<!-- header wrapper -->
+<div class="header-wrapper">	
+	<div class="nav-container">
+    	<div class="nav">
+		<div class="nav-right">
+        <ul>
+		  <li class="hover"><label class="drop-label" for="drop-wrap1"><img src="<?php echo $user->profileImage; ?>"/></label>
+		  <input type="checkbox" id="drop-wrap1">
+		    <div class="drop-wrap">
+		      <div class="drop-inner">
+		        <ul>
+		          <li><a href="home.php">Home</a></li>
+	              <li><a href="profileEdit.php">Profile</a></li>
+                  <li id="messagePopup"><a>Messages<span id="messages"><?php if($notify->totalM > 0){echo '<span class="span-i">'.$notify->totalM.'</span>';}?></span></a></li>
+                  <li><a href="account.php">Settings</a></li>
+				  <li><a href="logout.php">Log out</a></li>
+				</ul>
+		      </div>
+		    </div>
+		  </li>
+		  <li>
+            <input type="text" placeholder="Search" class= "search"/>
+		    <div class="search-result"></div>
+		  </li>
+		  <li>
+		    <label class="addTweetBtn">New Post</label></li>
+		</ul>
+		</div><!-- nav right ends-->
+	</div><!-- nav ends -->
+	</div><!-- nav container ends -->
+ </div><!-- header wrapper end -->
+ <!--Profile cover-->
+<div class="profile-cover-wrap"> 
+<div class="profile-cover-inner">
+	<div class="profile-cover-img">
+		<img src="<?php echo $profileData->profileCover;?>"/>
+	</div>
+</div>
+<div class="profile-nav">
+ <div class="profile-navigation">
+	<ul>
+		<li>
+		<div class="n-head">
+			TWEETS
+		</div>
+		<div class="n-bottom">
+		  <?php $getFromT->countTweets($profileId);?>
+		</div>
+		</li>
+		<li>
+			<a href="<?php echo $profileData->username;?>/following">
+				<div class="n-head">
+					<a href="<?php echo $profileData->username;?>/following">FOLLOWING</a>
+				</div>
+				<div class="n-bottom">
+					<span class="count-following"><?php echo $profileData->following;?></span>
+				</div>
+			</a>
+		</li>
+		<li>
+		 <a href="<?php echo $profileData->username;?>/followers">
+				<div class="n-head">
+					FOLLOWERS
+				</div>
+				<div class="n-bottom">
+					<span class="count-followers"><?php echo $profileData->followers;?></span>
+				</div>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<div class="n-head">
+					LIKES
+				</div>
+				<div class="n-bottom">
+					<?php $getFromT->countLikes($profileId);?>
+				</div>
+			</a>
+		</li>
+	</ul>
+	<div class="edit-button">
+		<span>
+			<?php echo $getFromF->followBtn($profileId, $user_id, $profileData->user_id);?>
+ 		</span>
+	</div>
+    </div>
+</div>
+</div><!--Profile Cover End-->
+
+<!---Inner wrapper-->
+<div class="in-wrapper"> 
+ <div class="in-full-wrap">
+   <div class="in-left">
+     <div class="in-left-wrap">
+	<!--PROFILE INFO WRAPPER END-->
+	<div class="profile-info-wrap">
+	 <div class="profile-info-inner">
+
+		<div class="profile-img">
+			<img src="<?php echo $profileData->profileImage;?>"/>
+		</div>	
+
+		<div class="profile-name-wrap">
+			<div class="profile-name">
+				<a href="<?php echo $profileData->username;?>"><?php echo $profileData->screenName;?></a>
+			</div>
+			<div class="profile-tname">
+				@<span class="username"><?php echo $profileData->username;?></span>
+			</div>
+		</div>
+
+		<div class="profile-bio-wrap">
+		 <div class="profile-bio-inner">
+		    <?php echo $profileData->bio;?>
+		 </div>
+		</div>
+
+<div class="profile-extra-info">
+	<div class="profile-extra-inner">
+		<ul>
+			<li>
+				<div class="profile-ex-location-i">
+					<i class="fa fa-map-marker" aria-hidden="true"></i>
+				</div>
+				<div class="profile-ex-location">
+					<?php echo $profileData->country;?>
+				</div>
+			</li>
+			<li>
+				<div class="profile-ex-location-i">
+					<i class="fa fa-link" aria-hidden="true"></i>
+				</div>
+				<div class="profile-ex-location">
+					<a href="#"><?php echo $profileData->website;?></a>
+				</div>
+			</li>
+			<li>
+				<div class="profile-ex-location-i">
+					<!-- <i class="fa fa-calendar-o" aria-hidden="true"></i> -->
+				</div>
+				<div class="profile-ex-location">
+ 				</div>
+			</li>
+			<li>
+				<div class="profile-ex-location-i">
+					<!-- <i class="fa fa-tint" aria-hidden="true"></i> -->
+				</div>
+				<div class="profile-ex-location">
+				</div>
+			</li>
+		</ul>						
+	</div>
+</div>
+
+<div class="profile-extra-footer">
+	<div class="profile-extra-footer-head">
+		<div class="profile-extra-info">
+			<ul>
+				<li>
+					<div class="profile-ex-location-i">
+						<i class="fa fa-camera" aria-hidden="true"></i>
+					</div>
+					<div class="profile-ex-location">
+						<a href="#">0 Photos and videos </a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="profile-extra-footer-body">
+		<ul>
+			 <!-- <li><img src="#"/></li> -->
+		</ul>		
+	</div>
+
+	<?php $getFromF->whoToFollow($user_id,$user_id);?>
+
+	<?php $getFromT->trends();?>
+</div>
+
+	 </div>
+	<!--PROFILE INFO INNER END-->
+
+	</div>
+	<!--PROFILE INFO WRAPPER END-->
+	<div class="popupTweet"></div>
+	</div>
+	<!-- in left wrap-->
+
+  </div>
+	<!-- in left end-->
+		<!--FOLLOWING OR FOLLOWER FULL WRAPPER-->
+		<div class="wrapper-following">
+			<div class="wrap-follow-inner">
+			  <?php $getFromF->followingList($profileId, $user_id, $profileData->user_id);?>		
+			</div><!-- wrap follo inner end-->
+		</div><!--FOLLOWING OR FOLLOWER FULL WRAPPER END-->
+		<script type="text/javascript" src="assets/js/follow.js"></script>
+		<script type="text/javascript" src="assets/js/notification.js"></script>
+		<script type="text/javascript" src="assets/js/hashtag.js"></script>
+		<script type="text/javascript" src="assets/js/search.js"></script>
+		<script type="text/javascript" src="assets/js/messages.js"></script>
+		<script type="text/javascript" src="assets/js/popupForm.js"></script>		
+		<script type="text/javascript" src="assets/js/postMessage.js"></script>		
+	</div><!--in full wrap end-->
+</div>
+<!-- in wrappper ends-->
+
+</div><!-- ends wrapper -->
+</body>
+</html>
