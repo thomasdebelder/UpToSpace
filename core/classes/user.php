bae0b40222d3dd0a89d13709753b9f389bbbfe06
Sarah Van Oers <vanoerssarah@gmail.com>
2018-05-14 19:22:28 +0200

    Feature 13 - tijd

--- core/classes/user.php
+++ core/classes/user.php
@@ -202,6 +202,40 @@ if($i < count($fields)) {
 $stmt->execute();
   }
 }
+/*
+  ------------------------
+          FEATURE 13 -- time
+  ------------------------        
+  */
+public function timeAgo($datetime) {
+  $time    = strtotime($datetime);
+   $current = time();
+   $seconds = $current - $time;
+   $minutes = round($seconds / 60);
+  $hours   = round($seconds / 3600);
+  $months  = round($seconds / 2600640);
+
+  if($seconds <= 60) {
+    if($seconds == 0) {
+      return 'now';
+    }
+        else {
+      return $seconds.'s';
+    }
+   }
+       else if($minutes <= 60) {
+        return $minutes.'m';
+       }
+      else if($hours <= 24) {
+        return $hours.'h';
+      }
+      else if($months <= 12) {
+        return date('M j', $time);
+      }
+      else {
+    return date('j M Y', $time);
+  }
+}
 
 } // end of User class
 ?>
