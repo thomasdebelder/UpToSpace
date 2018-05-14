c53b0dbdb7e74df4672aa3413402d73fb6d4dfe8
Sarah Van Oers <vanoerssarah@gmail.com>
2018-05-14 16:55:14 +0200

    update profileEdit

--- core/classes/user.php
+++ core/classes/user.php
@@ -113,6 +113,18 @@
     
     return $user->user_id;
   }
+  /*
+  ------------------------
+          EDIT USER PROFILE
+  ------------------------        
+  */
+  public function userData($user_id) {
+    $stmt = $this->pdo->prepare('SELECT * FROM `users` WHERE `user_id` = :user_id');
+    $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
 
+    return $stmt->fetch(PDO::FETCH_OBJ);
+}
+  
 } // end of User class
 ?>
